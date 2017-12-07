ae9e40a127dffb0b5e9967b4971f58757934b2c7
Pierre Chesne <pierrc@microsoft.com>
2017-12-07 11:07:25 +0100

    Workshop-3

---
+++ azure-quickstart-templates/Workshop-3/deploy.ps1
@@ -0,0 +1,33 @@
+﻿$AzureAccount = 'root@masociete.onmicrosoft.com'
+$PassAzureTenant= 'Password123$'
+
+#Login 'Tenant Azure'
+$PasswordAzureLogin = convertto-securestring $PassAzureTenant -asplaintext -force
+$Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $AzureAccount, $PasswordAzureLogin
+Login-AzureRmAccount -Credential $Credential
+
+#Get-AzureRmSubscription –SubscriptionName "Services de la plateforme Windows Azure pour Visual Studio Ultim" | Select-AzureRmSubscription
+
+$rgName ='RG_Name'
+$Location= 'Westeurope'
+
+try {     
+    Get-AzureRmResourceGroup -Name $rgName -Location $location -ErrorAction Stop     
+    Write-Host 'Le "ResourceGroup" existe déjà...' -foregroundcolor yellow 
+}
+catch {     
+New-AzureRmResourceGroup -Name $rgName -Location $location 
+}
+
+New-AzureRmResourceGroupDeployment `
+-Name DeployAppv1 `
+-ResourceGroupName $rgName `
+-TemplateFile .\azuredeploy.json `
+-TemplateParameterFile .\azuredeploy.parameters.json `
+-Verbose
+
+
+
+
+
+
