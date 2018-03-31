eb41ed7687e02ba6999384699885cdd0e4c279e5
Pierre Chesne <pierrc@microsoft.com>
2018-03-31 08:34:35 +0200

    Workshop-DSC-1

---
+++ azure-quickstart-templates/Workshop-DSC-1/MonSiteWebDSCPush.ps1
@@ -0,0 +1,73 @@
+﻿Configuration MonSiteWebDSCPush
+
+{
+    param(
+        [string]$repertoireContenuSite = "c:\ContenuSite",
+        [string]$repertoireZip = "c:\ContenuSite\zip",
+        [string]$site = "FourthCoffee.zip",
+        [string]$uri = "https://repositorydsc0.blob.core.windows.net/confdsc/FourthCoffee.zip",
+        [string]$nomdusite = "FourthCoffee"
+    )
+
+    Import-DscResource -ModuleName PsDesiredStateConfiguration, xPSDesiredStateConfiguration
+
+    WindowsFeature Role-IIS {
+        Ensure = "Present" 
+        Name   = "Web-WebServer"  
+    }
+
+    WindowsFeature WebServerManagementConsole {
+        Ensure    = "Present"
+        Name      = "Web-Mgmt-Console"            
+        DependsOn = "[WindowsFeature]Role-IIS"
+    }
+
+    WindowsFeature ASP45 {
+        Ensure = "Present"
+        Name   = "NET-Framework-45-ASPNET"                                     
+    }
+
+    WindowsFeature WCF {
+        Ensure    = "Present"
+        Name      = "NET-WCF-Services45"
+        DependsOn = "[WindowsFeature]ASP45"
+    }
+
+    WindowsFeature HttpActivation {
+        Ensure    = "Present"
+        Name      = "NET-WCF-HTTP-Activation45"
+        DependsOn = "[WindowsFeature]ASP45"
+    }
+
+    File Directory {
+        Ensure          = "Present"
+        Type            = "Directory"
+        DestinationPath = $repertoireZip                       
+    }
+
+    xRemoteFile DownloadSiteWebContent {
+        Uri             = $uri
+        DestinationPath = $repertoireZip
+        DependsOn       = "[File]Directory"
+    }
+
+    Archive ArchiveExample {
+        Ensure      = "Present" 
+        Path        = "$repertoireZip\$site"
+        Destination = "$repertoireContenuSite"
+        DependsOn   = "[xRemoteFile]DownloadSiteWebContent"
+    }
+
+    File SiteWebContent {
+        Ensure          = "Present"
+        Type            = "Directory"
+        Recurse         = $true
+        SourcePath      = "$repertoireContenuSite\$nomdusite"
+        DestinationPath = "c:\inetpub\wwwroot"
+        DependsOn       = "[Archive]ArchiveExample"
+    }     
+
+}
+
+MonSiteWebDSCPush -outputpath "C:\Mof"
+Start-DscConfiguration -Path "C:\Mof" -Verbose -Wait -Force
---
+++ azure-quickstart-templates/Workshop-DSC-1/README.md
@@ -0,0 +1,33 @@
+# Worshop PowerShell DSC . Création d'un site Web en mode 'Push'
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FPierre-Chesne%2Fazure%2Fmaster%2Fazure-quickstart-templates%2Workshop-DSC-1%2Fazuredeploysimple.json" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+<a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2FPierre-Chesne%2Fazure%2Fmaster%2Fazure-quickstart-templates%2FWorkshop-DSC-1%2Fazuredeploysimple.json" target="_blank">
+    <img src="http://armviz.io/visualizebutton.png"/>
+</a>
+
+
+
+Le template déploie pour un LAB :
+- Une IP Publique
+- Une URL
+- Un groupe de disponibilité
+- Un virtual network- 
+- Une VM Windows Server
+- Auto-Shutdown à 19h
+
+
+
+
+##Création d'un "Resource Group":
+New-AzureRmResourceGroup -Name $rgName -Location $location 
+
+
+##Déploiement de la VM:
+New-AzureRmResourceGroupDeployment `
+-Name DeployLab `
+-ResourceGroupName $rgName `
+-TemplateFile .\azuredeploy.json `
+-Verbose
+
